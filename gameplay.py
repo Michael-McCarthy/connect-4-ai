bdc0447346ca144b262c958f9af0ea609fa6a73e
Michael McCarthy <mictmccarthy@umass.edu>
2015-11-16 20:19:41 -0500

    Support for ai vs ai

--- gameplay.py
+++ gameplay.py
@@ -1,25 +1,37 @@
 import connect4
 import ai
 
-players = [1,2]
-
-
 if __name__=='__main__':
 
-    print "hi"
+    # Set this to 0 for ai vs ai games
+    human = 1
 
+
+    #Tracks if any player has won the game
     winner = False
 
-    player = 0
-    currentplayer = players[0]
+    currentplayer = 1
 
     board = connect4.real_board
 
+    #initalize a player 1 ai if no human
+    if human == 0:
+        ai1 = ai.ai(1)
+
+    #ai1.set_SELF_SCORE(10)
+
+    #initalize player 2 ai
+    ai2 = ai.ai(2)
+
+    #ai2.set_OPPONENT_SCORE(10)
+
+    #while no winner and there are still moves available
     while winner == False and len(connect4.open_columns(board)) > 0:
 
         connect4.print_board(board)
 
-        if currentplayer == 1:
+        #Human move
+        if currentplayer == 1 and human > 0:
 
             open_columns = connect4.open_columns(board)
 
@@ -35,27 +47,32 @@ if __name__=='__main__':
                 column = input("Player " + str(currentplayer) + " choose an open column to drop your piece in: \n")
 
             winner = connect4.drop_piece(column, currentplayer, board)
-
+        #ai player 1 move
+        elif currentplayer == 1:
+            column = ai1.analyze_choices(3, 1, board)[1]
+            print "DROP AT: " + str(column)
+            winner = connect4.drop_piece(column, currentplayer, board)
+        #ai player 2 move
         else:
-            column = ai.analyze_choices(5, 2, 2, board)[1]
+            column = ai2.analyze_choices(3, 2, board)[1]
             print "DROP AT: " + str(column)
             winner = connect4.drop_piece(column, currentplayer, board)
 
 
-
+        #if no winner, switch players
         if winner != True:
-            if player == 1:
-                player = 0
+            if currentplayer == 1:
+                currentplayer = 2
 
             else:
-                player = 1
+                currentplayer = 1
 
-            currentplayer = players[player]
-
+    #if filled board, draw
     if winner == False:
         print "DRAW"
         connect4.print_board(board)
 
+    #else print player who made the final move
     else:
         print "Player " + str(currentplayer) + " Wins!"
         connect4.print_board(board)
